7c7feb71033647bf055cb2ac0cc7435b94b3b377
Cam Crow <19ccrow99@gmail.com>
2023-05-05 08:49:28 -0500

    fix: cleaned up code

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from db.select import get_from_db
 from mail.build import build_week_html
 from mail.send_email import send_email
-from warn.scrapers import tx
-from warn.scrapers import ca
-from warn.scrapers import ny
-
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
 res = get_from_db()
 data = []
