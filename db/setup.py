002439075235839ea6463ae17e612d2dc82c795e
Cam Crow <19ccrow99@gmail.com>
2023-05-06 13:25:43 -0500

    feat: added arizona to db

--- db/setup.py
+++ db/setup.py
@@ -10,6 +10,7 @@ from state_data.scrape_ny import get_newyork_data
 from state_data.scrape_fl import get_florida_data
 from state_data.scrape_pa import get_pennsylvania_data
 from state_data.scrape_oh import get_ohio_data
+from state_data.scrape_az import get_arizona_data
 
 conn = psycopg2.connect(
    database=os.getenv('PG_DB'), user=os.getenv('PG_USER'), password=os.getenv('PG_PASS'), host='localhost', port= '5432'
@@ -54,3 +55,6 @@ insert_to_db(pa_data)
 
 oh_data = get_ohio_data()
 insert_to_db(oh_data)
+
+az_data = get_arizona_data()
+insert_to_db(az_data)
